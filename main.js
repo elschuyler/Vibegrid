8f50dac7eef8f4ec24b6a8d345be7ddb2392b098
elschuyler <elvilewis40@gmail.com>
2026-03-12 17:38:12 +0530

    Update main.js

--- main.js
+++ main.js
@@ -6,72 +6,12 @@ await Vault.init();
 renderLibrary();
 Controls.init();
 
-function renderLibrary() {
-  Vault.getAll().then(apps => {
-    const list = document.getElementById(
-      'lib-list');
-    list.innerHTML = apps.map(app => `
-      <div class="lib-item" 
-      onclick="launchApp('${app.id}')">
-        <div class="lib-icon"></div>
-        <div class="lib-info">
-          <div class="lib-name">
-          ${app.name}</div>
-          <div class="lib-meta">
-          ROMZ Package</div>
-        </div>
-        <button class="lib-del" 
-        onclick="event.stopPropagation(); 
-        deleteApp('${app.id}')">×</button>
-      </div>
-    `).join('');
-  });
-}
-
-window.launchApp = async (id) => {
-  const apps = await Vault.getAll();
-  const app = apps.find(a => a.id === id);
-  if (app) {
-    loadRomz(app.blob);
-    document.getElementById('library-view')
-      .classList.add('hidden');
-  }
+// Toggle the Numpad Grid
+window.toggleNumpad = () => {
+  const num = document.getElementById(
+    'numpad-overlay'
+  );
+  num.classList.toggle('hidden');
 };
 
-window.deleteApp = async (id) => {
-  if (confirm('Delete app?')) {
-    await Vault.remove(id);
-    renderLibrary();
-  }
-};
-
-window.showLibrary = () => {
-  document.getElementById('library-view')
-    .classList.toggle('hidden');
-};
-
-window.pickNewRomz = () => {
-  const input = document.createElement('input');
-  input.type = 'file';
-  input.accept = '.romz,.zip';
-  input.onchange = async (e) => {
-    const file = e.target.files[0];
-    if (file) {
-      const id = Date.now().toString();
-      await Vault.save(id, file.name, file);
-      renderLibrary();
-    }
-  };
-  input.click();
-};
-
-window.toggleMenu = () => {
-  document.getElementById('vibe-menu')
-    .classList.toggle('hidden');
-};
-
-window.quitGame = () => {
-  document.getElementById('vibe-screen')
-    .srcdoc = '';
-  window.toggleMenu();
-};
+// ... (Rest of previous main.js logic)
