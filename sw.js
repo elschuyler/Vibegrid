73653928f02fb8e9a1810c68bdbe40a3100492ea
elschuyler <elvilewis40@gmail.com>
2026-03-12 17:41:47 +0530

    Create sw.js

---
+++ sw.js
@@ -0,0 +1,25 @@
+const CACHE_NAME = 'vibe-v1';
+const ASSETS = [
+  './',
+  './index.html',
+  './style.css',
+  './main.js',
+  './romz-handler.js',
+  './vault.js',
+  './controls.js',
+  './manifest.json'
+];
+
+self.addEventListener('install', (e) => {
+  e.waitUntil(
+    caches.open(CACHE_NAME)
+      .then(cache => cache.addAll(ASSETS))
+  );
+});
+
+self.addEventListener('fetch', (e) => {
+  e.respondWith(
+    caches.match(e.request)
+      .then(res => res || fetch(e.request))
+  );
+});
